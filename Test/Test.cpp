374ba00dd885a20e949dcf473049036f406a7530
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2023-09-18 13:32:24 -0600

    Add project files.

---
+++ Test/Test.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{   
+
+    char heart = 3;
+    for (int l = 0; l < 100000; l++) {
+        int num = rand() % 20;
+        for (int j = 0; j < num; j++)
+            printf(heart);
+        cout << char(003);
+        Sleep(1);
+    }
+}
